d6108ed4db35e30bf466df13102c31a82dd61109
Oksana Ohorilko <ksnohorilko@gmail.com>
2020-03-01 13:15:27 +0200

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,24 @@
+#Adding:
+a = int(input("Please enter your first number: "))
+b = int(input("Please enter your second number: "))
+c = a + b
+print("Your result is " + str(c))
+
+#Subtracting:
+a = int(input("Please enter your first number: "))
+b = int(input("Please enter your second number: "))
+c = a - b
+print("Your result is " + str(c))
+
+
+#Multiplying:
+a = int(input("Please enter your first number: "))
+b = int(input("Please enter your second number: "))
+c = a * b
+print("Your result is " + str(c))
+
+#Dividing:
+a = int(input("Please enter your first number: "))
+b = int(input("Please enter your second number: "))
+c = a / b
+print("Your result is " + str(c))
